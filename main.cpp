36abc3c094dc9066343ce9ece325028df5376fe5
xRonnyn <pashuha.k@gmail.com>
2026-03-23 22:18:12 +0300

    can compile

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include "top-it-vector.hpp"
+#include <iostream>
+#include <iomanip>
+
+int testDefaultVector()
+{
+    topit::Vector< int > v;
+    return v.isEmpty();
+}
 
 int main()
 {
-
+    using test_t = bool(*)();
+    using pair_t = std::pair<const char*, test_t>;
+    pair_t tests[] = {
+        {"Default vector is empty", "testDefaultVector"}
+    };
+    const size_t count = sizeof(tests) / sizeof(pair_t);
+    std::cout<<std::boolalpha;
+    for(size_t i = 0; i < count; ++i)
+    {
+        bool res = tests[i]();
+        std::cout<< "Result: " << res << "\n";
+    }
 }
--- top-it-vector.hpp
+++ top-it-vector.hpp
@@ -6,9 +6,32 @@ namespace topit
     template< class T >
     struct Vector {
 
-    private:
-        T* data;
-        size_t size_, capacity_;
+        Vector();
+        Vector(const Vector< T >&) = delete;
+        Vector < T >& operator=(const Vector< T >&) = delete;
+        ~Vector();
+        bool isEmpty() const noexcept;
+        private:
+            T* data;
+            size_t size_, capacity_;
     };
+}
+
+template< class T >
+bool topit::Vector< T >::isEmpty() const noexcept
+{
+    return false;
+}
+
+template< class T >
+topit::Vector< T >::Vector():
+  data_(nullptr),
+  size_(0),
+  capacity_(0)
+{}
+template< class T >
+topit::Vector< T >::~Vector()
+{
+    delete[] data_;
 }
 #endif
