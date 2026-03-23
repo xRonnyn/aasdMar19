6355fc071308068db3237b19377affc896f6148d
xRonnyn <pashuha.k@gmail.com>
2026-03-23 23:47:41 +0300

    cg and fixes

--- main.cpp
+++ main.cpp
@@ -5,88 +5,91 @@
 
 int testDefaultVector()
 {
-    topit::Vector< int > v;
-    return v.isEmpty();
+  topit::Vector< int > v;
+  return v.isEmpty();
 }
 
 bool testVectorWithValue()
 {
-    topit::Vector< int > v;
-    v.pushBack(1);
-
+  topit::Vector< int > v;
+  v.pushBack(1);
 }
 
-bool testCopyConstructor(){
-    topit::Vector< int > v;
-    v.pushBack(1);
-    topit::Vector< int > yav = v;
-    bool isAllEqual = v.getSize() == yav.getSize();
-    for(size_t i = 0; isAllEqual && i < v.getSize(); ++i){
-        isAllEqual = isAllEqual && (v[i] == yav[i]);
-    }
-    return isAllEqual;
+bool testCopyConstructor()
+{
+  topit::Vector< int > v;
+  v.pushBack(1);
+  topit::Vector< int > yav = v;
+  bool isAllEqual = v.getSize() == yav.getSize();
+  for (size_t i = 0; isAllEqual && i < v.getSize(); ++i) {
+    isAllEqual = isAllEqual && (v[i] == yav[i]);
+  }
+  return isAllEqual;
 }
 
-bool testElementAccess(){
-    topit::Vector< int > v;
-    v.pushBack(1);
-    v.pushBack(2);
-    return v[0] == 1 && v[1] == 2;
+bool testElementAccess()
+{
+  topit::Vector< int > v;
+  v.pushBack(1);
+  v.pushBack(2);
+  return v[0] == 1 && v[1] == 2;
 }
-bool testSwap(){
-    topit::Vector< int > v;
-    v.pushBack(1);
-    topit::Vector< int > yav;
-    yav.pushBack(2);
-    v.swap(yav);
-    return yav[0] == 1 && v[0] == 2;
+bool testSwap()
+{
+  topit::Vector< int > v;
+  v.pushBack(1);
+  topit::Vector< int > yav;
+  yav.pushBack(2);
+  v.swap(yav);
+  return yav[0] == 1 && v[0] == 2;
 }
-bool testPushFront(){
-    topit::Vector< int > v;
-    v.pushFront(1);
-    v.pushFront(2);
-    return v[0] == 1 && v[1] == 2;
+bool testPushFront()
+{
+  topit::Vector< int > v;
+  v.pushFront(1);
+  v.pushFront(2);
+  return v[0] == 1 && v[1] == 2;
 }
-bool testEmptyCapacity(){
-    topit::Vector< int > v;
-    return !v.getCapacity();
+bool testEmptyCapacity()
+{
+  topit::Vector< int > v;
+  return !v.getCapacity();
 }
-bool testEmptySize(){
-    topit::Vector< int > v;
-    return !v.getSize();
+bool testEmptySize()
+{
+  topit::Vector< int > v;
+  return !v.getSize();
 }
-bool testPopBack(){
-    topit::Vector< int > v;
-    v.pushBack(1);
-    v.pushBack(2);
-    v.popBack();
-    return v.getSize() == 1 && v[0] == 1;
+bool testPopBack()
+{
+  topit::Vector< int > v;
+  v.pushBack(1);
+  v.pushBack(2);
+  v.popBack();
+  return v.getSize() == 1 && v[0] == 1;
 }
 
 int main()
 {
-    using test_t = bool(*)();
-    using pair_t = std::pair<const char*, test_t>;
-    pair_t tests[] = {
-        {"Default vector is empty", testDefaultVector},
-        {"Vector with any value is not empty", testVectorWithValue},
-        {"Inbound access elenets", testElementAccess},
-        {"Sizes must be equal as elements", testCopyConstructor},
-        {"Correct swap elements", testSwap},
-        {"The element should be added in the beginning",testPushFront},
-        {"Capacity of empty vector should be 0",testEmptyCapacity},
-        {"Size of empty vector should be 0",testEmptySize},
-        {"Last element should be deleted",testPopBack}
-    };
-    const size_t count = sizeof(tests) / sizeof(pair_t);
-    std::cout<<std::boolalpha;
-    bool pass = true;
-    for(size_t i = 0; i < count; ++i)
-    {
-        bool res = tests[i].second();
-        std::cout<<res<<" :";
-        std::cout<< tests[i].first<<"\n";
-        pass = pass && res;
-    }
-    std::cout<<pass<<": RESULT\n";
+  using test_t = bool (*)();
+  using pair_t = std::pair< const char *, test_t >;
+  pair_t tests[] = {{"Default vector is empty", testDefaultVector},
+                    {"Vector with any value is not empty", testVectorWithValue},
+                    {"Inbound access elenets", testElementAccess},
+                    {"Sizes must be equal as elements", testCopyConstructor},
+                    {"Correct swap elements", testSwap},
+                    {"The element should be added in the beginning", testPushFront},
+                    {"Capacity of empty vector should be 0", testEmptyCapacity},
+                    {"Size of empty vector should be 0", testEmptySize},
+                    {"Last element should be deleted", testPopBack}};
+  const size_t count = sizeof(tests) / sizeof(pair_t);
+  std::cout << std::boolalpha;
+  bool pass = true;
+  for (size_t i = 0; i < count; ++i) {
+    bool res = tests[i].second();
+    std::cout << res << " :";
+    std::cout << tests[i].first << "\n";
+    pass = pass && res;
+  }
+  std::cout << pass << ": RESULT\n";
 }
--- top-it-vector.hpp
+++ top-it-vector.hpp
@@ -1,129 +1,121 @@
 #ifndef TOPITVECTOR_HPP
 #define TOPITVECTOR_HPP
+#include <cassert>
+#include <stdexcept>
 #include <cstddef>
+#include <utility>
 namespace topit
 {
-    template< class T >
-    struct Vector {
-        Vector();
-        Vector(const Vector< T >&) = delete;
-        Vector < T >& operator=(const Vector< T >&) = delete;
-        ~Vector();
+  template < class T > struct Vector
+  {
+    Vector();
+    Vector(const Vector< T > &);
+    Vector< T > &operator=(const Vector< T > &);
+    ~Vector();
 
-        void swap(Vector< T >& rhs) noexcept;
+    void swap(Vector< T > &rhs) noexcept;
 
-        bool isEmpty() const noexcept;
-        size_t getSize() const noexcept;
-        size_t getCapacity() const noexcept;
+    bool isEmpty() const noexcept;
+    size_t getSize() const noexcept;
+    size_t getCapacity() const noexcept;
 
-        T& operator[](size_t id) noexcept;
-        const T& operator[](size_t id) const noexcept;
+    T &operator[](size_t id) noexcept;
+    const T &operator[](size_t id) const noexcept;
 
-        void pushBack(const T&);
-        void pushFront(const T&);
-        void popBack();
+    void pushBack(const T &);
+    void pushFront(const T &);
+    void popBack();
 
-    private:
-        T* data_;
-        size_t size_, capacity_;
+  private:
+    T *data_;
+    size_t size_, capacity_;
 
-        explicit Vector(size_t k);
-    };
+    explicit Vector(size_t k);
+  };
 }
-template < class T >
-void topit::Vector< T >::popBack(){
-  if (size_ == 0){
+template < class T > void topit::Vector< T >::popBack()
+{
+  if (size_ == 0) {
     throw std::out_of_range("Vector is empty");
   }
   size_--;
   data_[size_].~T();
 }
-template < class T >
-size_t topit::Vector< T >::getCapacity() const noexcept{
+template < class T > size_t topit::Vector< T >::getCapacity() const noexcept
+{
   return capacity_;
 }
 
 template < class T >
-void topit::Vector < T >::pushFront(const T &val) // сложный момент : в дефолт библиотеке вектор не реализовано
+void topit::Vector< T >::pushFront(const T &val) // сложный момент : в дефолт библиотеке вектор не реализовано
 {
-  Vector< T > cpy(val.getSize() + 1);
+  Vector< T > cpy(size_ + 1);
   cpy[0] = val;
-  for (size_t i = 1; i < cpy.getSize(); i++){
+  for (size_t i = 1; i < cpy.getSize(); i++) {
     cpy[i] = (*this)[i - 1];
   }
   swap(cpy);
 }
-template< class T >
-void topit::Vector< T > ::swap(Vector< T >& rhs) noexcept // обязательно noexcept (идиома copy-swap)
+template < class T > void topit::Vector< T >::swap(Vector< T > &rhs) noexcept // обязательно noexcept (идиома copy-swap)
 {
-    std::swap(cpy.data_, data_); //не может быть исключение ( все встроенные типы )
-    std::swap(cpy.size_, size_);
-    std::swap(cpy.capacity_, capacity_);
+  std::swap(rhs.data_, data_); // не может быть исключение ( все встроенные типы )
+  std::swap(rhs.size_, size_);
+  std::swap(rhs.capacity_, capacity_);
 }
-template < class T >
-topit::Vector< T >& topit::Vector< T >::operator=(const Vector< T >& rhs)
+template < class T > topit::Vector< T > &topit::Vector< T >::operator=(const Vector< T > &rhs)
 {
   Vector< T > cpy(rhs);
   swap(cpy);
   return *this;
 }
-template <class T>
-topit::Vector<T>::Vector(size_t k)
+template < class T > topit::Vector< T >::Vector(size_t k)
 {
-    data_(new T[k.size()]),
-    size_(k.getSize()),
-    capacity_(k.getSize())
+  data_(new T[k.size()]), size_(k.getSize()), capacity_(k.getSize())
 }
-template< class T >
-bool topit::Vector< T >::isEmpty() const noexcept
+template < class T > bool topit::Vector< T >::isEmpty() const noexcept
 {
-    return !size_;
+  return !size_;
 }
-template< class T >
-size_t topit::Vector< T >::getSize() const noexcept
+template < class T > size_t topit::Vector< T >::getSize() const noexcept
 {
   return size_;
 }
 
-template< class T >
-topit::Vector< T >::Vector(const Vector< T >& rhs):
-  Vector(rhs.getSize()) //ЗДесь мы пишем таким образом так как иначе надо будет писать catch (если создавать через поля просто) а так мы вызываем конструктор и деструктор вызовится
+template < class T >
+topit::Vector< T >::Vector(const Vector< T > &rhs):
+  Vector(rhs.getSize()) // ЗДесь мы пишем таким образом так как иначе надо будет писать catch (если создавать через поля
+                        // просто) а так мы вызываем конструктор и деструктор вызовится
 {
-  for (size_t i = 0; i < getSize(); ++i){
+  for (size_t i = 0; i < getSize(); ++i) {
     data_[i] = rhs[i];
   }
 }
-template< class T >
-topit::Vector< T >::~Vector()
+template < class T > topit::Vector< T >::~Vector()
 {
-    delete[] data_;
+  delete[] data_;
 }
-template< class T >
-T& topit::Vector< T >::operator[](size_t id) noexcept
+template < class T > T &topit::Vector< T >::operator[](size_t id) noexcept
 {
   assert(id < getSize());
   return data_[id];
 }
 
-template< class T >
-const T& topit::Vector< T >::operator[](size_t id) const noexcept
+template < class T > const T &topit::Vector< T >::operator[](size_t id) const noexcept
 {
   assert(id < getSize());
   return data_[id];
 }
 
-template< class T >
-void topit::Vector< T >::pushBack(const T&)
+template < class T > void topit::Vector< T >::pushBack(const T &val)
 {
-  if (size_ == capacity_){
-    size_t newCap *= 2;
-    T* new_data = new T[newCap];
-    try{
-      for (size_t i = 0; i < getSize(); ++i){
+  if (size_ == capacity_) {
+    size_t newCap = (capacity_ == 0 ? 1 : capacity_ * 2);
+    T *new_data = new T[newCap];
+    try {
+      for (size_t i = 0; i < getSize(); ++i) {
         new_data[i] = data_[i];
       }
-    }
-    catch(){
+    } catch (...) {
       delete[] new_data;
       throw;
     }
@@ -131,7 +123,7 @@ void topit::Vector< T >::pushBack(const T&)
     data_ = new_data;
     capacity_ = newCap;
   }
-  data_[size_++] = T;
+  data_[size_++] = val;
 }
 
 #endif
